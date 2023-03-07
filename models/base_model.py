4a784fd936afbb0fb5a77aa0e09c88185c03a5c6
Delali Funani <dfunani@live.co.za>
2023-03-07 07:30:12 -0800

    added tests

--- models/base_model.py
+++ models/base_model.py
@@ -12,12 +12,20 @@ class BaseModel():
     - Intepreter methods
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Constructor
         for the base class"""
-        self.id = uuid.uuid4()
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            try:
+                self.id = kwargs['id']
+                self.created_at = kwargs['created_at']
+                self.updated_at = kwargs['updated_at']
+            except KeyError as error:
+                print("Dictionary must Contain required attributes as Keys")
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def save(self):
         """Serializer
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -3,29 +3,29 @@ using unittesting"""
 import unittest
 from models.base_model import BaseModel
 from datetime import datetime
-import uuid
 
 class Test_BaseModel(unittest.TestCase):
     """ Class managing the test cases """
+    def setUp(self):
+        self.basemodel = BaseModel()
 
     def test_init(self):
         """ Tests creation of the base model and asserts if it exists """
-        basemodel = BaseModel()
-        self.assertIs(type(basemodel), BaseModel)
-        self.assertIs(type(basemodel.created_at), datetime)
-        self.assertIs(type(basemodel.updated_at), datetime)
-        self.assertIsInstance(basemodel.id, uuid.UUID)
+        # basemodel = BaseModel()
+        self.assertIs(type(self.basemodel), BaseModel)
+        self.assertIs(type(self.basemodel.created_at), datetime)
+        self.assertIs(type(self.basemodel.updated_at), datetime)
+        self.assertIsInstance(self.basemodel.id, str)
 
     def test_save(self):
-        basemodel = BaseModel()
-        start = basemodel.updated_at
-        basemodel.save()
-        end = basemodel.updated_at
+        start = self.basemodel.updated_at
+        self.basemodel.save()
+        end = self.basemodel.updated_at
         self.assertNotEqual(start, end)
 
     def test_dict(self):
-        basemodel = BaseModel()
-        res = basemodel.to_dict()
+        # basemodel = BaseModel()
+        res = self.basemodel.to_dict()
         self.assertIs(type(res), dict)
         self.assertIn('__class__', res)
         self.assertIn('id', res)
@@ -35,8 +35,8 @@ class Test_BaseModel(unittest.TestCase):
         self.assertIs(type(res['updated_at']), str)
 
     def test_str_rep(self):
-        basemodel = BaseModel()
-        self.assertRegex(str(basemodel), r'\[BaseModel\] \([a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}\) \{.+\}')
+        # basemodel = BaseModel()
+        self.assertRegex(str(self.basemodel), r'\[BaseModel\] \([a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}\) \{.+\}')
 
 if __name__ == "__main__":
     unittest.main()
